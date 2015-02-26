3a1e1dd20a4ec545349ae1ccc232d848430a815e
Brian Aggrey <baggrey1@gmail.com>
2015-02-25 22:24:32 -0800

    added colorwipe function to main call

--- LEDroom.py
+++ LEDroom.py
@@ -25,6 +25,13 @@ def updateAll(strip, color):
 		strip.setPixelColor(i,color)
 	strip.show()
 
+def colorWipe(strip, color, wait_ms=50):
+	"""Wipe color across display a pixel at a time."""
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, color)
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+
 if __name__ == '__main__':
 	strip1 = Adafruit_NeoPixel(STRIP_1_LED_COUNT, STRIP_1_GPIO_PIN, STRIP_1_LED_FREQ_HZ, STRIP_1_DMA, STRIP_1_BRIGHTNESS, STRIP_1_INVERT, STRIP_1_PWM_CHANNEL)
 	strip2 = Adafruit_NeoPixel(STRIP_2_LED_COUNT, STRIP_2_GPIO_PIN, STRIP_2_LED_FREQ_HZ, STRIP_2_DMA, STRIP_2_BRIGHTNESS, STRIP_2_INVERT, STRIP_2_PWM_CHANNEL)
@@ -33,5 +40,12 @@ if __name__ == '__main__':
 	strip2.begin()
 	updateAll(strip1, Color(255,0,0))
 	updateAll(strip2, Color(0,255,0))
+	while True:
+		colorWipe(strip1, Color(255,0,0))
+		colorWipe(strip2, Color(255,0,0))
+		colorWipe(strip1, Color(0,255,0))
+		colorWipe(strip2, Color(0,255,0))
+		colorWipe(strip1, Color(0,0,255))
+		colorWipe(strip2, Color(0,0,255))
 
 
