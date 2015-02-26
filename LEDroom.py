e081bcd1585af9373a91d85cd7cf9922040c1171
Brian Aggrey <baggrey1@gmail.com>
2015-02-25 22:37:58 -0800

    removed colorWipe

--- LEDroom.py
+++ LEDroom.py
@@ -1,7 +1,6 @@
 # Control neopixels from raspberry pi
 # Author: Brian Aggrey (baggrey1@gmail.com)
 
-import time
 from neopixel import *
 
 #LED strip configuration
@@ -26,12 +25,6 @@ def updateAll(strip, color):
 		strip.setPixelColor(i,color)
 	strip.show()
 
-def colorWipe(strip, color, wait_ms=50):
-	"""Wipe color across display a pixel at a time."""
-	for i in range(strip.numPixels()):
-		strip.setPixelColor(i, color)
-		strip.show()
-		time.sleep(wait_ms/1000.0)
 
 if __name__ == '__main__':
 	strip1 = Adafruit_NeoPixel(STRIP_1_LED_COUNT, STRIP_1_GPIO_PIN, STRIP_1_LED_FREQ_HZ, STRIP_1_DMA, STRIP_1_INVERT, STRIP_1_BRIGHTNESS, STRIP_1_PWM_CHANNEL)
@@ -39,14 +32,7 @@ if __name__ == '__main__':
 
 	strip1.begin()
 	strip2.begin()
-	updateAll(strip1, Color(255,0,0))
-	updateAll(strip2, Color(0,255,0))
-	while True:
-		colorWipe(strip1, Color(255,0,0))
-		colorWipe(strip2, Color(255,0,0))
-		colorWipe(strip1, Color(0,255,0))
-		colorWipe(strip2, Color(0,255,0))
-		colorWipe(strip1, Color(0,0,255))
-		colorWipe(strip2, Color(0,0,255))
+	updateAll(strip1, Color(255,128,255))
+	updateAll(strip2, Color(255,128,255))
 
 
