21b030cc2601f7bb2aa3f771f4a5f9c20478e2f6
Brian Aggrey <baggrey1@gmail.com>
2015-04-18 23:59:20 -0700

    removed namespace

--- bedroomServer.py
+++ bedroomServer.py
@@ -1,11 +1,9 @@
 from flask import Flask, request
 from flask.ext.socketio import SocketIO
 from LEDroom import boringOn, allOff, setColor
-from flask.ext.cors import CORS
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-cors = CORS(app)
 
 # define globals
 options = {
@@ -23,15 +21,15 @@ def state():
 
 	return input_list[0], 200
 
-@socketio.on('connect', namespace='/slider')
+@socketio.on('connect')
 def slider_connect():
     emit('my response', {'data': 'Connected!'})
 
-@socketio.on('json', namespace='/slider')
+@socketio.on('json')
 def set_colors(colors):
 	setColor(colors[0][intensity],colors[1][intensity],colors[2][intensity])	
 
-@socketio.on('disconnect', namespace='/slider')
+@socketio.on('disconnect')
 def slider_disconnect():
     print('Client disconnected')
 
