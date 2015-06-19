ac2090696bf5e2fed15bac253f694bcec7fafd0f
Brian Aggrey <baggrey1@gmail.com>
2015-06-19 00:08:32 -0700

    True?

--- bedroomServer.py
+++ bedroomServer.py
@@ -50,4 +50,4 @@ def slider_disconnect():
     print('Client disconnected')
 
 if __name__ == "__main__":
-	socketio.run(app, host='0.0.0.0', debug=true)
+	socketio.run(app, host='0.0.0.0', debug=True)
