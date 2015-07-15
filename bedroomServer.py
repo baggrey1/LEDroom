0b1316429c0e56c2ca1e46f79a2e00e42d2b2eab
Brian Aggrey <baggrey1@gmail.com>
2015-07-15 00:28:00 -0700

    moar hax

--- bedroomServer.py
+++ bedroomServer.py
@@ -18,7 +18,7 @@ def state():
 	input_list = options.get(command)
 	# Read previous command from text file
 	with open('last_command.txt') as infile:    
-		last_command = infile.read().strip(0)
+		last_command = infile.read().strip('0')
 
 	if command != last_command:		
 		functionToCall = input_list[1]
