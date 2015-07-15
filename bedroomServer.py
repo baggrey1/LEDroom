7104b41d224bf6adfa6e6e067d698242c40872aa
Brian Aggrey <baggrey1@gmail.com>
2015-07-14 23:57:35 -0700

    corrected bug in state file write line

--- bedroomServer.py
+++ bedroomServer.py
@@ -27,7 +27,7 @@ def state():
 
 	# Store command in text file
 	with open('last_command.txt','w') as outfile:
-		outfile.write("{}".format(command))
+		outfile.write(command)
 
 	return input_list[0], 200
 
