1a53c663a2f920fe9ffac56059c4842aac0e1b9e
Brian Aggrey <baggrey1@gmail.com>
2016-01-27 19:12:46 -0800

    Splitting http request server and socket.io into separate modules

---
+++ bedroomSocket.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask.ext.socketio import SocketIO
+from LEDroom import setColor
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@socketio.on('connect')
+def slider_connect():
+    emit('my response', {'data': 'Connected!'})
+    print('Client connected')
+
+@socketio.on('json')
+def set_colors(colors):
+	red = int(colors[0]['intensity'])
+	green = int(colors[1]['intensity'])
+	blue = int(colors[2]['intensity'])
+	setColor(red, green, blue)
+	storedColor = {
+		'red':red,
+		'green':green,
+		'blue': blue
+	}
+	with open('last_color.txt', 'w') as outfile:
+		json.dump(storedColor, outfile)
+
+@socketio.on('disconnect')
+def slider_disconnect():
+    print('Client disconnected')
+
+if __name__ == "__main__":
+	socketio.run(app, host='0.0.0.0')
